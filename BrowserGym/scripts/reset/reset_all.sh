f43ac9e25aa4e967063796f8a708d789049af2e7
AtsuMiyai <miyai.atsuyuki.practice@gmail.com>
2025-05-16 09:57:03 +0000

    remove extra info

--- BrowserGym/scripts/reset/reset_all.sh
+++ BrowserGym/scripts/reset/reset_all.sh
@@ -1,4 +1,4 @@
-source scripts/reset_zelda/reset_gitlab.sh
-source scripts/reset_zelda/reset_reddit.sh
-source scripts/reset_zelda/reset_shopping_admin.sh
-source scripts/reset_zelda/reset_shopping.sh
+source scripts/reset/reset_gitlab.sh
+source scripts/reset/reset_reddit.sh
+source scripts/reset/reset_shopping_admin.sh
+source scripts/reset/reset_shopping.sh
